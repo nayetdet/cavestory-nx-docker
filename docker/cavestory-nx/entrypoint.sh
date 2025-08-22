306893e0534d21f9d190a9114c2db6ee24b119e2
nayetdet <jpmoreiracampos21@gmail.com>
2025-08-21 23:25:28 -0300

    fix: ajustada posição do jogo na tela e melhorado desempenho do dockerfile

--- docker/cavestory-nx/entrypoint.sh
+++ docker/cavestory-nx/entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
-trap ctrl_c INT
-function ctrl_c() {
-  exit 0
-}
 rm /tmp/.X1-lock 2> /dev/null &
-/opt/noVNC/utils/launch.sh --vnc localhost:$VNC_PORT --listen $NO_VNC_PORT &
-echo -e "$TIGER_VNC_PASSWORD\n$TIGER_VNC_PASSWORD" | tigervncserver -xstartup ./cavestory-nx &
+/opt/novnc/utils/launch.sh --vnc localhost:5901 --listen 8080 &
+echo -e "$TIGER_VNC_PASSWORD\n$TIGER_VNC_PASSWORD" | tigervncserver -geometry $VNC_RESOLUTION -depth 24 :1 -xstartup ./cavestory-nx &
 wait
