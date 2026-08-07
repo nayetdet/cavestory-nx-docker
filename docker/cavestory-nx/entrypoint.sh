195280e8039ff42963e7921e3df64501e41a4fcd
nayetdet <jpmoreiracampos21@gmail.com>
2026-08-07 13:02:28 -0300

    feat: add initial project structure with Docker support and submodule integration

--- docker/cavestory-nx/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-rm /tmp/.X1-lock 2> /dev/null &
-/opt/novnc/utils/launch.sh --vnc localhost:5901 --listen 8080 &
-echo -e "$TIGER_VNC_PASSWORD\n$TIGER_VNC_PASSWORD" | tigervncserver -geometry $VNC_RESOLUTION -depth 24 :1 -xstartup ./cavestory-nx &
-wait
