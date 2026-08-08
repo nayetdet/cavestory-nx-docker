3b69f8ffa8bca20d3f091ae9fa81752431ea3cf9
nayetdet <jpmoreiracampos21@gmail.com>
2026-08-08 18:49:35 -0300

    fix: add missing logic to set default sink in entrypoint script

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -7,4 +7,11 @@ done
 
 selkies-resize "${SELKIES_MANUAL_WIDTH}x${SELKIES_MANUAL_HEIGHT}"
 
+until s6-setuidgid abc pactl set-default-sink output >/dev/null 2>&1; do
+    s6-setuidgid abc pactl load-module module-null-sink \
+        sink_name=output \
+        sink_properties=device.description=output >/dev/null 2>&1 || true
+    sleep 1
+done
+
 exec s6-setuidgid abc /opt/cavestory/cavestory-nx
