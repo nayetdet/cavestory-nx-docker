daac90c365e6e2348b2496ff1f42bd4823dcdf7d
nayetdet <jpmoreiracampos21@gmail.com>
2026-08-08 18:26:53 -0300

    fix: add settings file creation logic in entrypoint script

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -7,10 +7,4 @@ done
 
 selkies-resize "${SELKIES_MANUAL_WIDTH}x${SELKIES_MANUAL_HEIGHT}"
 
-settings_file="${HOME}/.local/share/cavestory-nx/nxengine/settings.dat"
-if [ -f "$settings_file" ] && [ "$(dd if="$settings_file" bs=1 count=4 2>/dev/null)" = NXS3 ]; then
-    printf '\10\0\0\0' | dd of="$settings_file" bs=1 seek=4 conv=notrunc status=none
-    printf '\1' | dd of="$settings_file" bs=1 seek=14 conv=notrunc status=none
-fi
-
 exec s6-setuidgid abc /opt/cavestory/cavestory-nx
